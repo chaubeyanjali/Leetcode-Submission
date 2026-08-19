7cfa43b2fedf8da83876af513d6c06cfea792bcb
Anjali Chaubey <anjalichaubey872@gmail.com>
2026-08-19 23:42:08 +0530

    Time: 11 ms (25.03%), Space: 24.7 MB (20.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,18 +3,16 @@ class Solution:
         freq = {}
 
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+            freq[num] = freq.get(num,0) + 1
 
         bucket = [[] for i in range(len(nums) + 1)]
 
-        for num, count in freq.items():
+        for num , count in freq.items():
             bucket[count].append(num)
 
         result = []
-
-        for i in range(len(bucket) - 1, 0, -1):
+        for i in range(len(nums) , 0, -1):
             for num in bucket[i]:
                 result.append(num)
-
                 if len(result) == k:
                     return result
